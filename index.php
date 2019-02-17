99fe85a1d03cef7b4fb7cd3fc206c5e502393de8
woodsleaf <woodsleaf@gmail.com>
2019-02-17 23:44:10 +0300

    first

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require 'vendor/autoload.php';
+use React\Http\Server;
+use React\Http\Response;
+use Psr\Http\Message\ServerRequestInterface;
+$loop = React\EventLoop\Factory::create();
+$server = new Server(
+    function (ServerRequestInterface $request) {
+        return new Response(
+            200,
+            ['Content-Type' => 'text/plain; charset=UTF-8'],
+            'Привет, мир'
+        );
+    }
+);
+$socket = new React\Socket\Server(8080, $loop);
+$server->listen($socket);
+echo 'Работает на '
+    . str_replace('tcp:', 'http:', $socket->getAddress())
+    . PHP_EOL;
+$loop->run();
